1aeecf2d2614fe44b17ef01f6910e4cb565f15e6
Martin Miranda <martinmirandac@gmail.com>
2017-07-16 15:36:21 +1000

    fix root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
-  get 'welcome/index'
-
   resources :articles
-  root 'welcome#index' 
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'articles#index'
 end
